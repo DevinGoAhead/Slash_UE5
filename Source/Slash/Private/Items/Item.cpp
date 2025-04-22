60d0f9d35345613aabf884c5415af6bfbf01869c
DevinWei <wxyback@outlook.com>
2025-04-22 15:08:40 +0800

    Draw debug shape on UE window

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Items/Item.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AItem::AItem() {
@@ -16,36 +17,25 @@ void AItem::BeginPlay() {
 	// 可能父类完成了一些功能
 	Super::BeginPlay();
 
-	// 输出到 UE output Log
-	// UE_LOG() 宏
-	// 日志类型, 详细等级
-	// TEXT(), 宏, 字符串字面量转换为 Unicode
-	UE_LOG(LogTemp, Warning, TEXT("AItem::BeginPlay() called!"));
+	UWorld * World = GetWorld();
+	if (World) {
+		FVector ActorLocation = GetActorLocation();
+		// Segment, 构成 shape 的网格的数量
+		// bPersistentLines, 若 true, 则永久存在不消失, 此时 LifeTime 参数失效
+		DrawDebugSphere(World, ActorLocation, 25.f, int32(32), FColor::Cyan, true);
 
-	// 输出到UE窗口
-	if (GEngine) {
-		// key, 与蓝图中key 含义相同, 同样的key值标记的输出内容会被覆盖
-		// TimeToDisplay, DeBugMessage 从显示到消失的时间, 相同key 的消息被覆盖后, TimeToDisplay 重置
-		// bNewerOnTop, 新的消息会显示在已有消息的上方, 只有当 key = INDEX_NONE 时, bNewerOnTop 才会生效
-		GEngine->AddOnScreenDebugMessage(3, 25.f, FColor::Blue, FString("AItem::Begin() output in screnn!"));
+		// 指向 Item 对象 单位方向向量方向, 长度为单位向量长度的100倍
+		// DepthPriority, 深度优先级, 覆盖优先级
+		DrawDebugLine(World, ActorLocation, ActorLocation + GetActorForwardVector() * FVector(100.f),
+					FColor::Yellow, true, -1.f, uint8(0U), 0.2f);
+		DrawDebugPoint(World, ActorLocation, 50.f, FColor::Green, true); // 起点
+		DrawDebugPoint(World, ActorLocation + GetActorForwardVector() * FVector(100.f), 50.f, FColor::Green, true); // 起点
 	}
+	
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
-	// 获取 class Item 实例化对象的名称
-	// 比如将一个 class 对象 place 到游戏中, 然后 alt 复制一份, 二者是不同的名字, 他们会调用各自的 AItem::Tick
-	// 进而在下面的调试输出函数中输出不同的名字
-	FString ItemName = GetName();
-
-	// Printf 可将内容格式化, 类似 sprintf
-	// *name, 返回 c 字符串, FString 重载了*, UE 中格式化 FString 必须使用*
-	FString OutPut = FString::Printf(TEXT("Item Name:, % s, Tick Time: % f"), *ItemName, DeltaTime);
-
-
-	UE_LOG(LogTemp, Warning, TEXT("%s"), *OutPut);
-	UE_LOG(LogTemp, Warning, TEXT("%2f"), DeltaTime); // 一个额外测试, 测试  TEXT
-	GEngine->AddOnScreenDebugMessage(1, 25.f, FColor::Magenta, OutPut);
 }
 
