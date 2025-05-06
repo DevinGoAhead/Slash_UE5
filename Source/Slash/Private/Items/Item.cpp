32c82f736fea6e7ae41645ea1afc5e1308200d0c
DevinWei <wxyback@outlook.com>
2025-05-06 12:17:51 +0800

    add OnSphereOverlap(begin and end) function in class AItem

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -3,18 +3,23 @@
 
 #include "Items/Item.h"
 #include "DrawDebugHelpers.h"
+#include "Components/SphereComponent.h"
 #include "../DebugMacros.h"
 
 // Sets default values
-AItem::AItem() {
+AItem::AItem() : TimeConstant(4.f), Amplitude(10.f){
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	// 如果 actor 不需要参与 tick, 例如 静态对象, 可以设置为 false
 	PrimaryActorTick.bCanEverTick = true;
 	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
 	RootComponent = ItemMesh; // 将 ItemMesh 设置为根组件
+
+	// 创建组件
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
 }
 
-float AItem::TransformedSin() const{
+float AItem::TransformedSin() const {
 	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
 }
 
@@ -22,17 +27,43 @@ float AItem::TransformedCos() const{
 	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
+void AItem::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex,
+	bool bFromSweep, 
+	const FHitResult& SweepResult) {
+
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(3, 15.f, FColor::Cyan, FString("Begin Overlap") + OtherActor->GetName());
+	}
+}
+
+void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex) {
+
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(4, 15.f, FColor::Blue, FString("End Overlap") + OtherActor->GetName());
+	}
+}
 // Called when the game starts or when spawned
 void AItem::BeginPlay() {
 	// Super::, 调用父类成员函	数
 	// 可能父类完成了一些功能
 	Super::BeginPlay();
+	if (Sphere) {
+		Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereBeginOverlap);
+		Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSphereEndOverlap);
+	}
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
-	
 	RunningTime += DeltaTime;
+
+	AddActorWorldOffset(FVector(0.f, 0.f, TransformedSin()));
 }
 
--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -6,10 +6,10 @@
 #include "GameFramework/Actor.h" // 这是 AActor 所在的头文件
 #include "Item.generated.h" // C++ 参与 UE 反射相关
 
+class USphereComponent;
 UCLASS()
 class SLASH_API AItem : public AActor {
-	GENERATED_BODY() // C++ 参与 UE 反射相关
-	
+	GENERATED_BODY() // C++ 参与 UE 反射相关	
 public:	
 	// Sets default values for this actor's properties
 	// 设置 Actor 的默认属性
@@ -26,6 +26,23 @@ protected:
 
 	UFUNCTION(BlueprintPure)
 	float TransformedCos() const;
+
+	UFUNCTION()
+	void OnSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+
+	UFUNCTION()
+	void OnSphereEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
 
 	// Called when the game starts or when spawned
 	// 游戏开始或被生成时调用, 虚函数
@@ -44,10 +61,13 @@ protected:
 	// 
 	// EditAnywhere, 在 BP Detail(蓝图窗口) 和 instance detail 中均可见并且可修改
 	// BlueprintReadWrite, 暴露给 blueprint graph, 可以 get, 可以 set
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float TimeConstant = 1.f; // 用于 RunningTime 的倍增系数
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move And Rotate")
+	float TimeConstant; // 用于 RunningTime 的倍增系数
 
 	// EditDefaultsOnly, 在 BP Detail(蓝图窗口) 可见, instance detail 中可见并且可修改
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float Amplitude = 1.f; // 振幅, 配合 sin cos 函数
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move And Rotate")
+	float Amplitude; // 振幅, 配合 sin cos 函数
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
 };
